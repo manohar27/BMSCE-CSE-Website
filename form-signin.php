87f1ddc10d382724c585942ab49c9aac5abfd3d2
manohar27 <mbmanohars@hotmail.com>
2014-09-17 23:25:08 +0530

    connecting to db

---
+++ form-signin.php
@@ -0,0 +1,23 @@
+<?php
+$email_id=$_POST['email_id'];
+$password=$_POST['pass'];
+$dbhost = "db4free.net";
+$db= "bmsnews";
+$user = "mbmanohars";
+$pass = "manu123S";
+$comm = mysql_connect($dbhost,$user,$pass);
+if(!$comm){
+ echo "Connection failed";
+}
+mysql_select_db($db);
+$stmt="select * from users where email like '" . $email_id . "' and passw like '" . $password . "'";
+$result= mysql_query($stmt);
+if($result==NULL){
+ die(mysql_error());
+ }
+while($line = mysql_fetch_array($result)){
+ echo "<h1>"."Welcome, "  . $line['name'] . "</h1>";
+ echo "<p> " ."Your username:". $line['email'] . "</p>";
+}
+?>
+
